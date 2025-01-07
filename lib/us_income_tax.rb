749c3f64d2ee4105740bc1b3cbbad4a3402bdce3
Jason Kim <iamjsonkim@gmail.com>
2025-01-06 23:56:31 -0800

    initial commit

---
+++ lib/us_income_tax.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "us_income_tax/version"
+
+module UsIncomeTax
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/us_income_tax/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module UsIncomeTax
+  VERSION = "0.1.0"
+end
